1c9c0c86943c548a2797c36b9484749a8456893f
Ma98hao04hsin15 <84980057+Ma98hao04hsin15@users.noreply.github.com>
2025-07-03 23:02:05 +0800

    Update IMDb2TMDB.js

--- widget/IMDb2TMDB.js
+++ widget/IMDb2TMDB.js
@@ -1,37 +1,46 @@
-WidgetMetadata = {
-  id: "forward.tmdb",
-  title: "TMDB",
-  version: "1.0.4",
+const WidgetMetadata = {
+  id: "forward.imdb.regionlist",
+  title: "IMDb 地区榜单",
+  version: "1.1.0",
   requiredVersion: "0.0.1",
-  description: "获取 TMDB 的榜单数据",
+  description: "根据地区和类型从 IMDb 获取榜单数据",
   author: "Forward",
   site: "https://github.com/InchStudio/ForwardWidgets",
   modules: [
     {
       id: "list",
-      title: "片单",
+      title: "地区片单",
       functionName: "list",
       params: [
         {
-          name: "url",
-          title: "列表地址",
+          name: "region",
+          title: "国家/地区代码",
           type: "input",
-          description: "IMDb 片单地址",
-          placeholders: [
-            {
-              title: "Most popular TV shows",
-              value: "https://www.imdb.com/chart/tvmeter/?ref_=nv_tvv_mptv",
-            }
+          description: "例如 us（美国）、kr（韩国）、jp（日本）",
+          default: "us",
+        },
+        {
+          name: "title_type",
+          title: "作品类型",
+          type: "select",
+          options: [
+            { title: "电视剧", value: "tv_series" },
+            { title: "电影", value: "feature" },
+            { title: "迷你剧", value: "tv_miniseries" },
+            { title: "纪录片", value: "documentary" },
           ],
-        }
+          default: "tv_series",
+        },
       ],
-    }
+    },
   ],
 };
-
 async function list(params = {}) {
-  let url = params.url;
-  console.log("请求 IMDb 片单页面:", url);
+  const region = params.region || "us";
+  const titleType = params.title_type || "tv_series";
+
+  const url = `https://www.imdb.com/search/title/?title_type=${titleType}&countries=${region}&sort=moviemeter,asc`;
+  console.log("请求 IMDb 高级搜索页面:", url);
 
   const response = await Widget.http.get(url, {
     headers: {
@@ -46,14 +55,11 @@ async function list(params = {}) {
   }
 
   const $ = Widget.html.load(response.data);
-  if (!$) {
-    throw new Error("解析 IMDb HTML 失败");
-  }
-
   const items = [];
-  $("table.chart tbody tr").each((_, element) => {
-    const link = $(element).find("td.titleColumn a").attr("href"); // e.g. "/title/tt0944947/"
-    const title = $(element).find("td.titleColumn a").text().trim();
+
+  $(".lister-item").each((_, element) => {
+    const link = $(element).find("h3.lister-item-header a").attr("href"); // /title/ttXXXXXX/
+    const title = $(element).find("h3.lister-item-header a").text().trim();
     const imdbIdMatch = link?.match(/\/title\/(tt\d+)\//);
     const imdbId = imdbIdMatch?.[1];
     if (imdbId) {
